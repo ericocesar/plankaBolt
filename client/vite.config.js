9c58efecca647068c8d9a5057d9f3ba68758124e
ericocesar <ericocesar@webck.com.br>
2026-01-07 16:29:20 -0300

    fix: add proxy to vite config for local development api calls

--- client/vite.config.js
+++ client/vite.config.js
@@ -25,6 +25,16 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1337',
+        changeOrigin: true,
+      },
+      '/attachments': {
+        target: 'http://localhost:1337',
+        changeOrigin: true,
+      },
+    },
   },
   build: {
     target: browserslistToEsbuild(['>0.2%', 'not dead', 'not op_mini all']),
